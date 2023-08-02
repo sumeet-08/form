f0e74bebf5a00d5eb466515d5cec41dd0879e857
sumeet-08 <139367538+sumeet-08@users.noreply.github.com>
2023-08-02 17:28:50 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#show-login").addEventListener("click",function(){
-    document.querySelector(".containers").classList.add("active");
-});
-
-document.querySelector(".containers .close-btn").addEventListener("click", function(){
-    document.querySelector(".containers").classList.remove("active");
-});
